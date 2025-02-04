39e299c237766167f589861c6845568a3043e04d
Kshitiz Kumar Maurya <kshitizkumarmaurya@Kshitizs-MacBook-Max.local>
2025-02-04 16:53:37 +0530

    ss

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,17 @@
 'use client';
 
-import Hero from "@/components/Home/Hero";
 import AboutUs from "@/components/Home/AboutUs";
-import Services from "@/components/Home/Services";
-import WhyChooseUs from "@/components/Home/WhyChooseUs";
-import SuccessStories from "@/components/Home/Success";
-import MeetOurExperts from "@/components/Home/MeetOurExperts";
 import BlogAndHealthTips from "@/components/Home/BlogAndHealthTips";
-import PartneredTeams from "@/components/Home/PartneredTeams";
-
 import ContactUs from "@/components/Home/ContactUs";
 import FAQ from "@/components/Home/Faq";
+import Hero from "@/components/Home/Hero";
+import MeetOurExperts from "@/components/Home/MeetOurExperts";
+import PartneredTeams from "@/components/Home/PartneredTeams";
+import Services from "@/components/Home/Services";
+import SuccessStories from "@/components/Home/Success";
+import WhyChooseUs from "@/components/Home/WhyChooseUs";
+
+
 
 export default function Home() {
   return (
