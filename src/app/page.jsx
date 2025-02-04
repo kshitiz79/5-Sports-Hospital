825070863d63dfbebbbf62781d0eeaa3c0110442
Kshitiz Kumar Maurya <kshitizkumarmaurya@Kshitizs-MacBook-Max.local>
2025-02-04 16:34:07 +0530

    Removed large file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,29 @@
-
-import AboutUs from "@/components/Home/AboutUs";
-import BlogAndHealthTips from "@/components/Home/BlogAndHealthTips";
-import ContactUs from "@/components/Home/ContactUs";
-import FAQ from "@/components/Home/Faq";
+'use client';
 
 import Hero from "@/components/Home/Hero";
-import MeetOurExperts from "@/components/Home/MeetOurExperts";
-import PartneredTeams from "@/components/Home/PartneredTeams";
+import AboutUs from "@/components/Home/AboutUs";
 import Services from "@/components/Home/Services";
-import SuccessStories from "@/components/Home/Success";
 import WhyChooseUs from "@/components/Home/WhyChooseUs";
-import Image from "next/image";
+import SuccessStories from "@/components/Home/Success";
+import MeetOurExperts from "@/components/Home/MeetOurExperts";
+import BlogAndHealthTips from "@/components/Home/BlogAndHealthTips";
+import PartneredTeams from "@/components/Home/PartneredTeams";
+import FAQ from "@/components/Home/FAQ";
+import ContactUs from "@/components/Home/ContactUs";
 
 export default function Home() {
   return (
     <div>
-      <Hero/>
-      <AboutUs/>
-      <Services/>
-      <WhyChooseUs/>
-      <SuccessStories/>
-      <MeetOurExperts/>
-      <BlogAndHealthTips/>
-      <PartneredTeams/>
-  
-      <FAQ/>
-      <ContactUs/>
+      <Hero />
+      <AboutUs />
+      <Services />
+      <WhyChooseUs />
+      <SuccessStories />
+      <MeetOurExperts />
+      <BlogAndHealthTips />
+      <PartneredTeams />
+      <FAQ />
+      <ContactUs />
     </div>
   );
 }
