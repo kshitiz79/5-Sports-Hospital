261103fc5895cdcccbe1efcc51d95946206bcd06
ManupriyaNagar <manupriyanagar2@gmail.com>
2025-05-05 16:55:25 +0530

    next

--- src/app/programs/surgical/page.jsx
+++ src/app/programs/surgical/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function SurgicalPrehabRehab() {
   return (
-    <section className="bg-gray-800 py-16 px-4 md:px-16">
+    <section className="bg-gray-800y py-16 px-4 md:px-16">
       <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-10 items-center">
         <div className="space-y-6">
           <h2 className="text-4xl font-bold text-gray-100">A Full-Circle Support System for Surgical Success</h2>
